ba447cfb0e7a62f6af82ec4071265f249926b8d7
pagerme123 <122313567+pagerme123@users.noreply.github.com>
2025-12-14 06:13:09 +0000

    font fixes removeing some deprevated things

--- hide.html.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (window.location.pathname.endsWith(".html")) {
-        var newPath = window.location.pathname.replace(".html", "");
-        window.history.pushState({}, "", newPath);
-    }
-});
